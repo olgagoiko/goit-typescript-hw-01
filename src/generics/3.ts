75386c7467fa988a3c0c1417fbce478a898458b7
Olga Goiko <olga.a10@ukr.net>
2024-11-01 20:13:44 +0200

    done

--- src/generics/3.ts
+++ src/generics/3.ts
@@ -2,6 +2,5 @@ function merge<T, U>(objA: T, objB: U) {
   return Object.assign({}, objA, objB);
 }
 
-const merged = merge({ name: 'Alisa' }, { age: 28 });
-
+const merged = merge({ name: 'Olha' }, { age: 28 });
 merged.name;
